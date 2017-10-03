4d0cc073bb7cc48fb7ef1bf4bc97b9ed12631440
Vbayrasheva <32329206+Vbayrasheva@users.noreply.github.com>
2017-10-03 21:48:31 +0300

    Update Task_10_g.java

--- Task/src/ru/itis/Task_10_g.java
+++ Task/src/ru/itis/Task_10_g.java
@@ -14,11 +14,12 @@ public class Task_10_g {
         for (k = 2; k <= 2 * n; k++ ){
             if (k % 2 == 0) {
                 s = s + Math.pow(x, k) / fact_chet;
-                fact_chet = fact_chet * (fact_chet + 2);
+                fact_chet = fact_chet * (fact_chet + 2); //странная формула
             }else{
-                fact_nechet = fact_nechet * (fact_nechet + 2);
+                fact_nechet = fact_nechet * (fact_nechet + 2);//странная формула
                 s = s + Math.pow(x, k ) / fact_nechet;
         }
+            //Остальные замечания см предыдущую задачу
         }
         System.out.println(s + x);
     }
