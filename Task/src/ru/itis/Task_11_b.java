73f77829da938d637d6b99a22729707373968597
Vbayrasheva <32329206+Vbayrasheva@users.noreply.github.com>
2017-10-03 21:50:54 +0300

    Update Task_11_b.java

--- Task/src/ru/itis/Task_11_b.java
+++ Task/src/ru/itis/Task_11_b.java
@@ -8,20 +8,20 @@ public class Task_11_b {
     static int n = 6;
 
     static double cosMult(double i){
-        for (int n = 1; n <= i; n++){
+        for (int n = 1; n <= i; n++){  // Плохо
             multCos = multCos * Math.cos(n);
         }
         return multCos;
     }
     static double sinMult(double i){
-        for(int n = 1; n <= i; n++){
+        for(int n = 1; n <= i; n++){ //  Плохо
             multSin = multSin * Math.sin(n);
         }
         return multSin;
     }
 
     public static void main(String[] args) {
-        sum = (cosMult(i) / sinMult(i)) * (n + 1);
+        sum = (cosMult(i) / sinMult(i)) * (n + 1); // О чем речь?
         System.out.println(sum);
     }
 
