5783367b9656358aed443db2e9bf2120eb463437
Aydar710 <aydar710@gmail.com>
2017-10-23 16:12:45 +0300

    Homework on 25 okt

---
+++ Homework_on_25_okt/src/ru/itis/Ex_23_b.java
@@ -0,0 +1,24 @@
+package ru.itis;
+// 23 б
+public class Ex_23_b {
+    static double x[] = {1.54, 2.25, 3.14, 4.3, 5, 5.5};
+    static double s[] = new double[x.length];
+    static double a = x[0];
+    static double b = x[x.length - 1];
+    static double h = (b - a) / (s.length - 1);
+    static int n = 0;
+
+
+    public static void main(String[] args) {
+        for (int k = 0; k < s.length; k++) {
+            n = 0;
+            for (int i = 0; i < x.length; i++) {
+                if ((x[i] >= a + ((double)i - 1) * h) && (x[i] < a + (double) i * h))
+                    n++;
+                s[k] = n;
+            }
+        }
+        for (double x : s)
+            System.out.print(x + " ");
+    }
+}
---
+++ Homework_on_25_okt/src/ru/itis/Ex_24_a.java
@@ -0,0 +1,30 @@
+package ru.itis;
+
+import java.util.Scanner;
+
+public class Ex_24_a {
+    static Scanner in = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        for (int n = 1; n <= 30; n++) {
+            int m = 2 * n + 1;
+            int fibon[] = new int[m + 1];
+            fibon[0] = 0;
+            fibon[1] = 1;
+
+            for (int i = 2; i < fibon.length; i++) {
+                fibon[i] = fibon[i - 1] + fibon[i - 2];
+            }
+            int Fm = 0;
+            for (int i = 2; i < fibon.length; i += 2) {
+                Fm += fibon[i];
+            }
+            Fm += 1;
+            if (Fm == fibon[fibon.length - 1])
+                System.out.println("Для n = " + n + " - верно");
+            else
+                System.out.println("Для n = " + n + " - не верно");
+        }
+    }
+}
