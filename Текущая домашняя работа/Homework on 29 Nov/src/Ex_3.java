1207379446eaf38d70ad26d81a536c3c44d49298
Vbayrasheva <32329206+Vbayrasheva@users.noreply.github.com>
2017-11-28 22:43:44 +0300

    Update Ex_3.java

--- Текущая домашняя работа/Homework on 29 Nov/src/Ex_3.java
+++ Текущая домашняя работа/Homework on 29 Nov/src/Ex_3.java
@@ -1,4 +1,4 @@
-public class Ex_3 {
+public class Ex_3 {//?
     public static void main(String[] args) {
         int a [][] = {{1, 2, 3}, {2, 3, 1}, {1, 3}, {1, 2, 3}};
         boolean f = true;
@@ -6,7 +6,7 @@ public class Ex_3 {
         for (int i = 0; i < a.length - 1; i++){
             for (int j = i + 1; j < a.length && !m; j++){
                 if (f && j != 1)
-                    m = true;
+                    m = true;                                 //I did not understand your idea
                 else
                     f = true;
                 for (int k = 0; k < a[i].length && f && a[i].length == a[j].length; k++){
